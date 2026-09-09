b4269f23902d43d1f3cfd41938444fb442719888
aliism <aliism@metropolia.fi>
2026-09-09 12:09:30 +0300

    Pelitehtävää

--- Peliprojekti/pelitehtävä01.py
+++ Peliprojekti/pelitehtävä01.py
@@ -1,30 +1,46 @@
 def nayta_inventory(lista):
-    for i in lista:
-        print(i)
+        if len(lista) == 0:
+            print("Sinulla ei ole vielä mitään mukana :(\n")
+        else:
+            for i in lista:
+                print("Sinulla on mukana seuraavat esineet: ")
+                print(i)
 
-def lisaa_inventoryyn(lista, esine):
-    lisaa = input("Kyllä vai ei?: ")
-    if lisaa == "Kyllä":
-        lista.append(esine)
-        print("Miekka lisätty inventaarioon")
+def lisaa_kapy_inventoryyn(lista, kapy):
+    kapy = input("Otetaanko käpy mukaan? Kyllä vai ei?: ")
+    if kapy == "Kyllä":
+        lista.append("Käpy")
+        print("Käpy lisätty inventaarioon")
 
-def nayta_health(elamapisteet):
-    print(elamapisteet)
+def lisaa_omena_inventoryyn(lista, omena):
+    omena = input("Otetaanko omena mukaan? Kyllä vai ei?: ")
+    if omena == "Kyllä":
+        lista.append("Omena")
+        print("Omena lisätty inventaarioon")
 
+def nayta_energy(energiapisteet):
+    print(f"Sinulla on {energiapisteet} energiapistettä!\n")
+
+## Ensimmäinen tehtävä
 def aloita_peli():
     print("Aloitetaan peli!\n")
     print("Edessäsi on risteys. Voit kääntyä (A) vasemmalle tai (D) oikealle.\n")
     valinta = input("valitse A tai D: ")
     if valinta == "A":
-        print("Käännyit vasemmalle ja löysit miekan! Otetaanko miekka mukaan?")
-        lisaa_inventoryyn(lista)
+        print("Käännyit vasemmalle ja löysit kävyn!")
+        lisaa_kapy_inventoryyn(lista, "Käpy")
     elif valinta == "D":
-        print("Käännyit oikealle ja löysin omenan! Otetaanko omena mukaan? ")
+        print("Käännyit oikealle ja löysin omenan!")
+        lisaa_omena_inventoryyn(lista, "Omena")
 
-max_elamapisteet = 10
-elamapisteet = 10
+# def seuraava_vaihe():
+
+
+max_energiapisteet = 10
+energiapisteet = 10
 lista = []
 
+## Ensimmäinen printti
 nimi = input("Mikä on nimesi?: ")
 ika = int(input("Kuinka vanha olet?: "))
 
@@ -35,17 +51,19 @@ if ika < 12:
 else:
     print(f"Terve {nimi}! Olet {ika} vuotias.\n")
 
-    valikko = (input("----VALIKKO----\nMitä haluat tehdä?\n(A) Aloittaa pelin\n(B) Katsoa inventaariota\n(C) Katsoa elämäpisteet\n"))
+    ## Aloitus valikko
+    valikko = (input("----VALIKKO----\nMitä haluat tehdä?\n(A) Aloittaa pelin\n(B) Katsoa inventaariota\n(C) Katsoa energiapisteet\n"))
+
     while valikko != "Lopeta":
         if valikko == "B":
             ## Näytä inventaario eli tavara lista funktiota käyttäen
             nayta_inventory(lista)
-            valikko = (input("----VALIKKO----\nMitä haluat tehdä?\n(A) Aloittaa pelin\n(B) Katsoa inventaariota\n(C) Katsoa elämäpisteet\n"))
+            valikko = (input("----VALIKKO----\nMitä haluat tehdä?\n(A) Aloittaa pelin\n(B) Katsoa inventaariota\n(C) Katsoa energiapisteet\n"))
 
         elif valikko == "C":
             ## Näytä elämäpisteet eli health funktiota käyttäen
-            nayta_health(elamapisteet)
-            valikko = (input("----VALIKKO----\nMitä haluat tehdä?\n(A) Aloittaa pelin\n(B) Katsoa inventaariota\n(C) Katsoa elämäpisteet\n"))
+            nayta_energy(energiapisteet)
+            valikko = (input("----VALIKKO----\nMitä haluat tehdä?\n(A) Aloittaa pelin\n(B) Katsoa inventaariota\n(C) Katsoa energiapisteet\n"))
 
         elif valikko == "A":
             aloita_peli()
@@ -53,7 +71,7 @@ else:
 
         else:
             print("Virheellinen syöte, yritä uudelleen\n")
-            valikko = (input("----VALIKKO----\nMitä haluat tehdä?\n(A) Aloittaa pelin\n(B) Katsoa inventaariota\n(C) Katsoa elämäpisteet\n"))
+            valikko = (input("----VALIKKO----\nMitä haluat tehdä?\n(A) Aloittaa pelin\n(B) Katsoa inventaariota\n(C) Katsoa energiapisteet\n"))
     
     print("Lopetetaan peli.")
         
--- Peliprojekti/readme.md
+++ Peliprojekti/readme.md
@@ -1,2 +1,6 @@
 **Pelin nimi**
 **Aliisa Majuri**
+
+Metsä seikkailu
+
+Kuljemme metsässä joko vasemmalle, oikealle tai suoraan. pelissä hyödynnetään luonnon antimia ruoaksi, jotta nälkä ei laske liian alas. Löydämme matkalla oravan joka on jäänyt jumiin lankaan. Meidän täytyy etsiä sakset, jotta voimme vapauttaa oravan.
